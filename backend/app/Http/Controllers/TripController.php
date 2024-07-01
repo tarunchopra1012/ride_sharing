9d1ce5bbe5eb7a4f5e9472d88e1073611945669e
tarunchopra1012 <tarun.chopra10@gmail.com>
2024-07-01 15:15:09 +0530

    Added Events and websockets

--- backend/app/Http/Controllers/TripController.php
+++ backend/app/Http/Controllers/TripController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\TripAccepted;
+use App\Events\TripEnded;
+use App\Events\TripLocationUpdated;
+use App\Events\TripStarted;
 use App\Models\Trip;
 use Illuminate\Http\Request;
 
@@ -9,6 +13,7 @@ class TripController extends Controller
 {
     public function store(Request $request)
     {
+        // dd($request->all());
         $request->validate([
             'origin' => 'required',
             'destination' => 'required',
@@ -57,6 +62,8 @@ class TripController extends Controller
 
         $trip->load('driver.user');
 
+        TripAccepted::dispatch($trip, $request->user());
+
         return $trip;
     }
 
@@ -69,6 +76,8 @@ class TripController extends Controller
 
         $trip->load('driver.user');
 
+        TripStarted::dispatch($trip, $request->user());
+
         return $trip;
     }
 
@@ -80,6 +89,8 @@ class TripController extends Controller
         ]);
 
         $trip->load('driver.user');
+
+        TripEnded::dispatch($trip, $request->user());
 
         return $trip;
     }
@@ -96,6 +107,8 @@ class TripController extends Controller
         ]);
 
         $trip->load('driver.user');
+
+        TripLocationUpdated::dispatch($trip, $request->user());
 
         return $trip;
     }
